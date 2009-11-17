14f8082ec20f254cfada7562e35c3b2d35b20c11
Sylvain Viollon <thefunny@gmail.com>
2009-11-17 07:54:23 +0100

    Add test infra

---
+++ src/zeam/utils/text/tests/test_doctest.py
@@ -0,0 +1,24 @@
+
+import unittest
+
+from zope.testing import doctest
+from grokcore.component.testing import grok_component
+
+from zeam.utils.text.testing import FunctionalLayer, setUp, tearDown
+
+def test_suite():
+    optionflags = doctest.NORMALIZE_WHITESPACE | doctest.ELLIPSIS
+    globs= {'grok_component': grok_component}
+
+    suite = unittest.TestSuite()
+    for filename in ['fields.txt',]:
+        test = doctest.DocFileSuite(
+            filename,
+            optionflags=optionflags,
+            setUp=setUp,
+            tearDown=tearDown,
+            globs=globs)
+        test.layer = FunctionalLayer
+        suite.addTest(test)
+
+    return suite
